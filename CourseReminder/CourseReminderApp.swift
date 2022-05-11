245a551a78709524cd74c20388e0acf86ed7e25a
Nicole Huang <nhuang@lcs.on.ca>
2022-05-11 12:44:54 -0400

    Initial Commit

---
+++ CourseReminder/CourseReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseReminderApp.swift
+//  CourseReminder
+//
+//  Created by Nicole on 2022/5/11.
+//
+
+import SwiftUI
+
+@main
+struct CourseReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
